d3515a6834ba0b6ca3f1cf42ae5d40dba614e57b
TravisPing <lawkwanping@gmail.com>
2018-04-29 23:07:39 +0800

    cha nge dashboard tablepageCtrl

--- src/app/pages/tables/TablesPageCtrl.js
+++ src/app/pages/tables/TablesPageCtrl.js
@@ -22,27 +22,49 @@
 
     
     $scope.smartTableData=window.smartTableData;
-    $.getJSON( "https://api.fixer.io/latest?base=USD", function(data  ) {
-
-      console.log('json: ',data.rates);
+    $.ajax({
+        url: "https://api.fixer.io/latest?base=USD",
+        async: false,
+        success: function(data){
+          console.log('json: ',data.rates);
        
-      for (var x in data.rates){
+          for (var x in data.rates){
 
-        var a = {
-          'id':++count,
-          'key':x,
-          'value':data.rates[x],
-          'valuePlus10':(data.rates[x]+10.0002).toFixed(4),
-          'even':(((data.rates[x]+10.0002).toFixed(4))*10000).toFixed(0)%2 == 0
+            var a = {
+              'id':++count,
+              'key':x,
+              'value':data.rates[x],
+              'valuePlus10':(data.rates[x]+10.0002).toFixed(4),
+              'even':(((data.rates[x]+10.0002).toFixed(4))*10000).toFixed(0)%2 == 0
 
+            }
+            list.push(a);
+          }
+          $scope.smartTableData =list; 
+          console.log('arr: ',$scope.smartTableData);
         }
-        list.push(a);
-      }
-      $scope.smartTableData =list; 
-      console.log('arr: ',$scope.smartTableData);
-
-
     });
+    // $.getJSON( "https://api.fixer.io/latest?base=USD", function(data  ) {
+
+    //   console.log('json: ',data.rates);
+       
+    //   for (var x in data.rates){
+
+    //     var a = {
+    //       'id':++count,
+    //       'key':x,
+    //       'value':data.rates[x],
+    //       'valuePlus10':(data.rates[x]+10.0002).toFixed(4),
+    //       'even':(((data.rates[x]+10.0002).toFixed(4))*10000).toFixed(0)%2 == 0
+
+    //     }
+    //     list.push(a);
+    //   }
+    //   $scope.smartTableData =list; 
+    //   console.log('arr: ',$scope.smartTableData);
+
+
+    // });
     
 
     $scope.smartTablePageSize = 10;
@@ -52,213 +74,39 @@
 
    // $scope.editableTableData = $scope.smartTableData.slice(0, 36);
 
-    $scope.peopleTableData = [
-      {
-        id: 1,
-        firstName: 'Mark',
-        lastName: 'Otto',
-        username: '@mdo',
-        email: 'mdo@gmail.com',
-        age: '28',
-        status: 'info'
-      },
-      {
-        id: 2,
-        firstName: 'Jacob',
-        lastName: 'Thornton',
-        username: '@fat',
-        email: 'fat@yandex.ru',
-        age: '45',
-        status: 'primary'
-      },
-      {
-        id: 3,
-        firstName: 'Larry',
-        lastName: 'Bird',
-        username: '@twitter',
-        email: 'twitter@outlook.com',
-        age: '18',
-        status: 'success'
-      },
-      {
-        id: 4,
-        firstName: 'John',
-        lastName: 'Snow',
-        username: '@snow',
-        email: 'snow@gmail.com',
-        age: '20',
-        status: 'danger'
-      },
-      {
-        id: 5,
-        firstName: 'Jack',
-        lastName: 'Sparrow',
-        username: '@jack',
-        email: 'jack@yandex.ru',
-        age: '30',
-        status: 'warning'
-      }
-    ];
+    
 
-    $scope.metricsTableData = [
-      {
-        image: 'app/browsers/chrome.svg',
-        browser: 'Google Chrome',
-        visits: '10,392',
-        isVisitsUp: true,
-        purchases: '4,214',
-        isPurchasesUp: true,
-        percent: '45%',
-        isPercentUp: true
-      },
-      {
-        image: 'app/browsers/firefox.svg',
-        browser: 'Mozilla Firefox',
-        visits: '7,873',
-        isVisitsUp: true,
-        purchases: '3,031',
-        isPurchasesUp: false,
-        percent: '28%',
-        isPercentUp: true
-      },
-      {
-        image: 'app/browsers/ie.svg',
-        browser: 'Internet Explorer',
-        visits: '5,890',
-        isVisitsUp: false,
-        purchases: '2,102',
-        isPurchasesUp: false,
-        percent: '17%',
-        isPercentUp: false
-      },
-      {
-        image: 'app/browsers/safari.svg',
-        browser: 'Safari',
-        visits: '4,001',
-        isVisitsUp: false,
-        purchases: '1,001',
-        isPurchasesUp: false,
-        percent: '14%',
-        isPercentUp: true
-      },
-      {
-        image: 'app/browsers/opera.svg',
-        browser: 'Opera',
-        visits: '1,833',
-        isVisitsUp: true,
-        purchases: '83',
-        isPurchasesUp: true,
-        percent: '5%',
-        isPercentUp: false
-      }
-    ];
+    // $scope.showGroup = function(user) {
+    //   if(user.group && $scope.groups.length) {
+    //     var selected = $filter('filter')($scope.groups, {id: user.group});
+    //     return selected.length ? selected[0].text : 'Not set';
+    //   } else return 'Not set'
+    // };
 
-    $scope.users = [
-      {
-        "id": 1,
-        "name": "Esther Vang",
-        "status": 4,
-        "group": 3
-      },
-      {
-        "id": 2,
-        "name": "Leah Freeman",
-        "status": 3,
-        "group": 1
-      },
-      {
-        "id": 3,
-        "name": "Mathews Simpson",
-        "status": 3,
-        "group": 2
-      },
-      {
-        "id": 4,
-        "name": "Buckley Hopkins",
-        "group": 4
-      },
-      {
-        "id": 5,
-        "name": "Buckley Schwartz",
-        "status": 1,
-        "group": 1
-      },
-      {
-        "id": 6,
-        "name": "Mathews Hopkins",
-        "status": 4,
-        "group": 2
-      },
-      {
-        "id": 7,
-        "name": "Leah Vang",
-        "status": 4,
-        "group": 1
-      },
-      {
-        "id": 8,
-        "name": "Vang Schwartz",
-        "status": 4,
-        "group": 2
-      },
-      {
-        "id": 9,
-        "name": "Hopkin Esther",
-        "status": 1,
-        "group": 2
-      },
-      {
-        "id": 10,
-        "name": "Mathews Schwartz",
-        "status": 1,
-        "group": 3
-      }
-    ];
-
-    $scope.statuses = [
-      {value: 1, text: 'Good'},
-      {value: 2, text: 'Awesome'},
-      {value: 3, text: 'Excellent'},
-    ];
-
-    $scope.groups = [
-      {id: 1, text: 'user'},
-      {id: 2, text: 'customer'},
-      {id: 3, text: 'vip'},
-      {id: 4, text: 'admin'}
-    ];
-
-    $scope.showGroup = function(user) {
-      if(user.group && $scope.groups.length) {
-        var selected = $filter('filter')($scope.groups, {id: user.group});
-        return selected.length ? selected[0].text : 'Not set';
-      } else return 'Not set'
-    };
-
-    $scope.showStatus = function(user) {
-      var selected = [];
-      if(user.status) {
-        selected = $filter('filter')($scope.statuses, {value: user.status});
-      }
-      return selected.length ? selected[0].text : 'Not set';
-    };
+    // $scope.showStatus = function(user) {
+    //   var selected = [];
+    //   if(user.status) {
+    //     selected = $filter('filter')($scope.statuses, {value: user.status});
+    //   }
+    //   return selected.length ? selected[0].text : 'Not set';
+    // };
 
 
-    $scope.removeUser = function(index) {
-      $scope.users.splice(index, 1);
-    };
+    // $scope.removeUser = function(index) {
+    //   $scope.users.splice(index, 1);
+    // };
 
-    $scope.addUser = function() {
-      $scope.inserted = {
-        id: $scope.users.length+1,
-        name: '',
-        status: null,
-        group: null
-      };
-      $scope.users.push($scope.inserted);
-    };
+    // $scope.addUser = function() {
+    //   $scope.inserted = {
+    //     id: $scope.users.length+1,
+    //     name: '',
+    //     status: null,
+    //     group: null
+    //   };
+    //   $scope.users.push($scope.inserted);
+    // };
 
-    $scope.onTimerTick = function() {
+    function onTimerTick() {
       $.getJSON( "https://api.fixer.io/latest?base=USD", function(data  ) {
         list=[];
         count=0;
@@ -284,13 +132,13 @@
         
       });
     }
-
+    setInterval(onTimerTick, 3300);
     editableOptions.theme = 'bs3';
     editableThemes['bs3'].submitTpl = '<button type="submit" class="btn btn-primary btn-with-icon"><i class="ion-checkmark-round"></i></button>';
     editableThemes['bs3'].cancelTpl = '<button type="button" ng-click="$form.$cancel()" class="btn btn-default btn-with-icon"><i class="ion-close-round"></i></button>';
 
    
-    setInterval($scope.onTimerTick(), 3300); // 33 milliseconds = ~ 30 frames per sec
+     // 33 milliseconds = ~ 30 frames per sec
 
   }
 
